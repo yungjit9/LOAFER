0f006075d823f47714370d5f2b15a4945f9c0e95
yungjit9 <77458316+yungjit9@users.noreply.github.com>
2021-01-16 13:40:45 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LOAFER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("planet jupiter");
+
+            int a = 600000;
+            int b = 3;
+            int c = 0;
+
+            c = a * b;
+            Console.WriteLine(c);
+            
+        }
+    }
+}
