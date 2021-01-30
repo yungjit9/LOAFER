28c960f0bdea5b8cc4cff5fc7b52db987da24860
yungjit9 <77458316+yungjit9@users.noreply.github.com>
2021-01-29 23:59:18 -0700

    Add files via upload

---
+++ Myclass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BakeryShoppingCart
+{
+    internal class Myclass<T>
+    {
+        public string Myproperty { get; internal set; }
+        public int Myproperty2 { get; internal set; }
+
+        internal void PrintSomething()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void PrintSomethingElse(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
